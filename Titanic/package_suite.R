1691733c22cc6fa5cab56543024270ebcbfa4eab
minhsphuc12 <phucnm.ec@gmail.com>
2016-11-24 15:00:51 +0700

    Signed-off-by: minhsphuc12 <phucnm.ec@gmail.com>

---
+++ Titanic/package_suite.R
@@ -0,0 +1,11 @@
+install.packages(c('installr','benchmarkme','bitops','markdown','rmarkdown','devtools'))
+install.packages(c('knitr','htmltools','caTools','testthat '))
+install.packages(c('rio','data.table','ggplot2','profvis'))
+
+
+
+
+library(installr,benchmarkme,bitops,markdown,rmarkdown,devtools)
+library(knitr,htmltools,caTools,testthat)
+library(rio,data.table,ggplot2,profvis)
+
